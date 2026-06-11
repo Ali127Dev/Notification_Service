eabf5351e8b06bc3b759b55f6cd1c7562e6b3ffb
Ali127Dev <AliMoradi0Business@gmail.com>
2026-06-11 09:51:42 +0330

    feat(usecase): implement process notification

---
+++ internal/application/usecase/process_notification.go
@@ -0,0 +1,50 @@
+package usecase
+
+import (
+	"github.com/Ali127Dev/Notification_Service/internal/domain/port"
+)
+
+type ProcessNotification struct {
+	repo   port.NotificationRepository
+	sender port.NotificationSender
+}
+
+func NewProcessNotification(
+	repo port.NotificationRepository,
+	sender port.NotificationSender,
+) *ProcessNotification {
+	return &ProcessNotification{repo: repo, sender: sender}
+}
+
+type ProcessNotificationRequest struct {
+	NotificationID string
+}
+
+func (uc *ProcessNotification) Execute(req ProcessNotificationRequest) error {
+	notification, err := uc.repo.FindByID(req.NotificationID)
+	if err != nil {
+		return err
+	}
+
+	if !notification.IsPending() {
+		return nil
+	}
+
+	notification.IncrementAttempts()
+	err = uc.sender.Send(notification)
+	if err != nil {
+		notification.MarkAsFailed()
+
+		if saveErr := uc.repo.Update(notification); saveErr != nil {
+			return saveErr
+		}
+
+		return err
+	}
+
+	if err := notification.MarkAsSent(); err != nil {
+		return err
+	}
+
+	return uc.repo.Update(notification)
+}
