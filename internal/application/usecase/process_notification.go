20ab832880580885327125439911d90190fd9752
Ali127Dev <AliMoradi0Business@gmail.com>
2026-06-11 10:18:34 +0330

    feat(usecase): implement retry strategy in process notification

--- internal/application/usecase/process_notification.go
+++ internal/application/usecase/process_notification.go
@@ -1,19 +1,22 @@
 package usecase
 
 import (
+	"github.com/Ali127Dev/Notification_Service/internal/application/retry"
 	"github.com/Ali127Dev/Notification_Service/internal/domain/port"
 )
 
 type ProcessNotification struct {
-	repo   port.NotificationRepository
-	sender port.NotificationSender
+	repo          port.NotificationRepository
+	sender        port.NotificationSender
+	consumerRetry *retry.Runner
 }
 
 func NewProcessNotification(
 	repo port.NotificationRepository,
 	sender port.NotificationSender,
+	consumerRetry *retry.Runner,
 ) *ProcessNotification {
-	return &ProcessNotification{repo: repo, sender: sender}
+	return &ProcessNotification{repo: repo, sender: sender, consumerRetry: consumerRetry}
 }
 
 type ProcessNotificationRequest struct {
@@ -31,7 +34,9 @@ func (uc *ProcessNotification) Execute(req ProcessNotificationRequest) error {
 	}
 
 	notification.IncrementAttempts()
-	err = uc.sender.Send(notification)
+	err = uc.consumerRetry.Do(func() error {
+		return uc.sender.Send(notification)
+	})
 	if err != nil {
 		notification.MarkAsFailed()
 
