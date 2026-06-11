ed59cbb5a5cf8e967b333ba853e98a54b1819bcb
Ali127Dev <AliMoradi0Business@gmail.com>
2026-06-11 09:17:44 +0330

    feat(port): implement notification repository

--- internal/domain/port/notification.go
+++ internal/domain/port/notification.go
@@ -5,3 +5,14 @@ import "github.com/Ali127Dev/Notification_Service/internal/domain/entity"
 type NotificationSender interface {
 	Send(*entity.Notification) error
 }
+
+type NotificationRepository interface {
+	Save(*entity.Notification) error
+	Update(*entity.Notification) error
+
+	FindByID(id string) (*entity.Notification, error)
+}
+
+type NotificationProducer interface {
+	Publish(*entity.Notification) error
+}
